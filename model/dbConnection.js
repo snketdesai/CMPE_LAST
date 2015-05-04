cbba0f148e8a0c021032da22facda6cb6035087d
purval <purval1989@gmail.com>
2015-05-03 19:32:37 -0700

    mongo module

--- model/dbConnection.js
+++ model/dbConnection.js
@@ -6,7 +6,8 @@ var pool;
 
 var redis = require("redis");
 var client = redis.createClient(6379,"127.0.0.1");
-//redis-cmpe282.cysnho.0001.usw1.cache.amazonaws.com
+//127.0.0.1
+//redis-come282.qqsr0p.0001.use1.cache.amazonaws.com
 
 function getRedisConnection(){
 	return client;
